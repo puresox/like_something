dfcd9461fffc1ecbb9166b07ef7cd19bcc2baafd
puresox <13004384112@163.com>
2016-10-30 10:59:58 +0800

    test

--- js/options.js
+++ js/options.js
@@ -1,9 +1,12 @@
 var user = localStorage.user || 0;
-var num = localStorage.num || 0;			
+var num = localStorage.num || 0;	
+var choice = localStorage.choice || 0;		
 document.getElementById('user').value = user;
 document.getElementById('num').value = num;
+document.getElementsByName('qq').value=choice;
 document.getElementById('save').onclick = function(){
     localStorage.user = document.getElementById('user').value;
     localStorage.num = document.getElementById('num').value;
+    localStorage.choice=document.document.getElementsByName('qq').value;
     alert('保存成功。');
 }
