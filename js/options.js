16a394b1591a4adb2df7196b0437e68973a449be
puresox <13004384112@163.com>
2016-10-31 23:21:51 +0800

    change options.js

--- js/options.js
+++ js/options.js
@@ -1,12 +1,18 @@
 var user = localStorage.user || 0;
 var num = localStorage.num || 0;	
-var choice = localStorage.choice || 0;		
+var mode= document.getElementsByName('mode');
+var mode_send;
+ for (var i = 0; i < mode.length; i++) 
+ {
+     if (mode[i].checked == true) 
+     {
+         mode_send=mode[i].value;
+    }
+}
 document.getElementById('user').value = user;
 document.getElementById('num').value = num;
-document.getElementsByName('qq').value=choice;
 document.getElementById('save').onclick = function(){
     localStorage.user = document.getElementById('user').value;
     localStorage.num = document.getElementById('num').value;
-    localStorage.choice=document.document.getElementsByName('qq').value;
     alert('保存成功。');
 }
