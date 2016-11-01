fc0100f739828eb3ad5dc6b373b690f6b27f542d
10011100000 <623807914@qq.com>
2016-11-01 21:57:53 +0800

    Fix bug and Add notes
    修复了模式传值的BUG
    添加了部分备注

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
     var all_ifo = new Array();
-    all_ifo[0]=localStorage.mode;
-    all_ifo[1]=localStorage.user;
-    all_ifo[2]=localStorage.like_mode;
+    all_ifo[0]=localStorage.mode;		//点赞模式
+    all_ifo[1]=localStorage.user;		//Q号
+    all_ifo[2]=localStorage.like_mode;		//指定QQ下的点赞模式
     sendResponse(all_ifo);
 });
--- js/options.js
+++ js/options.js
@@ -1,17 +1,20 @@
+//name:like_mode 指定QQ模式下的点赞/不点赞
+//name:mode 点赞模式
+
 var user = localStorage.user || 0;
 var like_mode=document.getElementsByName('like_mode');
 var like_mode_send;
 console.log('hello world');
 var mode= document.getElementsByName('mode');
 var mode_send;
-document.getElementById('user').value = user;
+document.getElementById('user').value = user;           //在输入QQ的框里显示已保存的QQ号
 document.getElementById('save').onclick = function(){
      for (var i = 0; i < mode.length; i++)
      {
-         if (mode[i].checked == true)
-         {
-             mode_send=mode[i].value;
-            }
+        if (mode[i].checked == true)
+        {
+            mode_send=mode[i].value;
+        }
     }
      for (i=0;i<like_mode.length;i++) 
      {                                          //下拉框的长度就是它的选项数.
@@ -23,5 +26,13 @@ document.getElementById('save').onclick = function(){
     localStorage.user = document.getElementById('user').value;
     localStorage.like_mode = like_mode_send;
     localStorage.mode=mode_send;
+
+    //测试数值
+    console.log(like_mode_send);
+    console.log(mode_send);
+    console.log(localStorage.user);
+    console.log(localStorage.like_mode);
+    console.log(localStorage.mode);
+
     alert('保存成功。');
 }
