e1cfad198dccbceab5517356e8603296abfe0a2a
puresox <13004384112@163.com>
2016-11-01 21:18:26 +0800

    2016/11/1   21.18

--- js/like_auto.js
+++ js/like_auto.js
@@ -1,13 +1,20 @@
 var like=document.getElementsByClassName('item qz_like_btn_v3')[0];
 var user;
-var choice;
-function presslike()
+var mode;
+var like_mode;
+function presslike_mode0()
+{
+
+}
+
+function presslike_mode1()
 {
 	var n=document.getElementsByClassName('f-user-avatar q_namecard f-s-a');
 	var i;
 	chrome.runtime.sendMessage('This string has no use', function(response){
-		user=response[0];
-		choice=response[1];
+		mode=response[0];
+		user=response[1];
+		like_mode=response[2];
 	});
 
 	for(i=0;i<n.length;i++)
@@ -25,7 +32,13 @@ function presslike()
 	}
 }
 
-function changeLikeButton(i)
+function changeLikeButton_mode0(i)
+{
+	document.getElementsByClassName('item qz_like_btn_v3')[i].innerHTML="测试点赞";
+	document.getElementsByClassName('item qz_like_btn_v3')[i].style.color="red";
+}
+
+function changeLikeButton_mode1(i)
 {
 	document.getElementsByClassName('item qz_like_btn_v3')[i].innerHTML="已通过插件点赞";
 	document.getElementsByClassName('item qz_like_btn_v3')[i].style.color="red";
