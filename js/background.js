7c68d6ffe5e95a44f65110ad1a88d4d357811d4a
puresox <13004384112@163.com>
2016-11-01 21:09:35 +0800

    test

--- js/background.js
+++ js/background.js
@@ -2,6 +2,6 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
     var all_ifo = new Array();
     all_ifo[0]=localStorage.mode;
     all_ifo[1]=localStorage.user;
-    all_ifo[2]=localStorage.num;
+    all_ifo[2]=localStorage.like_mode;
     sendResponse(all_ifo);
 });
--- js/options.js
+++ js/options.js
@@ -1,20 +1,27 @@
 var user = localStorage.user || 0;
-var num = localStorage.num || 0;
-console.log("123");	
+var like_mode=document.getElementsByName('like_mode');
+var like_mode_send;
+console.log('hello world');
 var mode= document.getElementsByName('mode');
-var mode_send=localStorage.mode || 0;
- for (var i = 0; i < mode.length; i++) 
- {
-     if (mode[i].checked == true) 
-     {
-         mode_send=mode[i].value;
-    }
-}
+var mode_send;
 document.getElementById('user').value = user;
-document.getElementById('num').value = num;
 document.getElementById('save').onclick = function(){
+     for (var i = 0; i < mode.length; i++)
+     {
+         if (mode[i].checked == true)
+         {
+             mode_send=mode[i].value;
+            }
+    }
+     for (i=0;i<like_mode.length;i++) 
+     {                                          //下拉框的长度就是它的选项数.
+         if (like_mode[i].selected== true ) 
+         {
+            like_mode_send=like_mode[i].value;        //获取当前选择项的值 .
+        }
+    }
     localStorage.user = document.getElementById('user').value;
-    localStorage.num = document.getElementById('num').value;
+    localStorage.like_mode = like_mode_send;
     localStorage.mode=mode_send;
     alert('保存成功。');
 }
