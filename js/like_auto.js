93000940679957ed71e3ac2602ab3abfd33fa227
puresox <13004384112@163.com>
2016-11-01 21:25:50 +0800

    21.25

--- js/like_auto.js
+++ js/like_auto.js
@@ -2,6 +2,12 @@ var like=document.getElementsByClassName('item qz_like_btn_v3')[0];
 var user;
 var mode;
 var like_mode;
+chrome.runtime.sendMessage('This string has no use', function(response){
+	mode=response[0];
+	user=response[1];
+	like_mode=response[2];
+});
+
 function presslike_mode0()
 {
 
@@ -10,14 +16,8 @@ function presslike_mode0()
 function presslike_mode1()
 {
 	var n=document.getElementsByClassName('f-user-avatar q_namecard f-s-a');
-	var i;
-	chrome.runtime.sendMessage('This string has no use', function(response){
-		mode=response[0];
-		user=response[1];
-		like_mode=response[2];
-	});
 
-	for(i=0;i<n.length;i++)
+	for(var i=0;i<n.length;i++)
 	{
 		if(n[i].getAttribute('href')=='http://user.qzone.qq.com/'+user)
 		{
@@ -44,4 +44,12 @@ function changeLikeButton_mode1(i)
 	document.getElementsByClassName('item qz_like_btn_v3')[i].style.color="red";
 }
 
-setInterval("presslike()",3000);
+if(mode==0)
+{
+	setInterval("presslike_mode0()",3000);
+}
+else
+{
+	setInterval("presslike_mode1()",3000);
+}
+
